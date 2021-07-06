37a3137f10b59af5f7fa01a93911f36ae37ae8a6
adbenoit-9 <adeline.benoit@hotmail.com>
2021-07-06 19:12:46 +0200

    align scop

--- inc/iter/Iterators_traits.hpp
+++ inc/iter/Iterators_traits.hpp
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 18:24:40 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/07/06 19:11:07 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/07/06 19:12:33 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ class iterator_traits
 	    typedef typename Iterator::value_type		value_type;
     	typedef typename Iterator::pointer			pointer;
     	typedef typename Iterator::reference		reference;
-    	typedef std::random_access_iterator_tag	iterator_category;
+    	typedef std::random_access_iterator_tag		iterator_category;
 };
 
 #endif
