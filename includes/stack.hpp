ca16635521c0ea53d51c7ba01d9e8b4693fe44d8
adbenoit-9 <adeline.benoit@hotmail.com>
2021-09-28 20:06:41 +0200

    stack move

--- includes/stack.hpp
+++ includes/stack.hpp
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 14:08:07 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/09/28 19:54:56 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/09/28 20:06:27 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ namespace ft
 			typedef	T			value_type;
 			typedef size_t		size_type;
 			
-			explicit stack(const container_type& ctnr = container_type())	{ this->_c = ctnr; }
+			explicit stack(const container_type& ctnr = container_type()) : _c(ctnr) {}
 			
 			bool                empty() const				{ return this->_c.empty(); }
 			size_type           size() const				{ return this->_c.size(); }
