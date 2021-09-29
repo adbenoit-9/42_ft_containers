b1ff8cfcba58cd0f03f501d1229cfbf9a9661a7d
adbenoit-9 <adeline.benoit@hotmail.com>
2021-09-29 17:07:04 +0200

    alloctor_type::reference truc chelou en tree const

--- includes/Tree.hpp
+++ includes/Tree.hpp
@@ -6,7 +6,7 @@
 /*   By: adbenoit <adbenoit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 15:43:23 by adbenoit          #+#    #+#             */
-/*   Updated: 2021/09/29 16:28:52 by adbenoit         ###   ########.fr       */
+/*   Updated: 2021/09/29 17:05:38 by adbenoit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,16 +94,16 @@ namespace ft
 		 **/
 			
 		public:
-			typedef typename T::first_type 						key_type;
-			typedef typename T::second_type 					mapped_type;
-			typedef T											value_type;
-			typedef	Compare										key_compare;
-			typedef	Alloc										allocator_type;
-			typedef typename allocator_type::reference			reference;
-			typedef typename allocator_type::const_reference	const_reference;
-			typedef	typename allocator_type::pointer			pointer;
-			typedef	typename allocator_type::const_pointer		const_pointer;
-			typedef	size_t										size_type;
+			typedef typename T::first_type 		key_type;
+			typedef typename T::second_type 	mapped_type;
+			typedef T							value_type;
+			typedef	Compare						key_compare;
+			typedef	Alloc						allocator_type;
+			typedef value_type&					reference;
+			typedef const value_type&			const_reference;
+			typedef	value_type*					pointer;
+			typedef	const value_type*			const_pointer;
+			typedef	size_t						size_type;
 
 			struct Node
 			{
